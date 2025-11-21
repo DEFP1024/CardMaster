c1575331d8018c160f13379f918aa504686c366c
ForestPool <zxpqzx123456@gmail.com>
2025-11-21 15:13:04 +0900

    [feat] 카드 ScriptableObject로 구현

---
+++ Assets/Scripts/Card/CardData.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CardData", menuName = "Scriptable Objects/CardData")]
+public class CardData : ScriptableObject, ICard
+{
+    [Header("기본 정보")]
+    [Tooltip("카드 이름")]
+    [SerializeField] private string cardName;
+    [Tooltip("카드 타입")]
+    [SerializeField] private CardType cardType;
+    [Tooltip("카드 코스트")]
+    [Min(-1)]
+    [SerializeField] private int cost;
+    [Tooltip("카드 힘(공격력, 아머수치, 스킬 파워)")]
+    [SerializeField] private int power;
+    [Tooltip("카드 설명")]
+    [TextArea]
+    [SerializeField] private string description;
+
+
+
+    public string Name => cardName;
+
+    public int Cost => cost;
+
+    public int Power => power;
+
+    public string Description => description;
+
+    public CardType Type => cardType;
+}
