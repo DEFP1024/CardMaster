48b229a3ee828678752c7840ead3d91fdf9e318a
ForestPool <zxpqzx123456@gmail.com>
2025-11-21 13:44:48 +0900

    [feat] Title화면에서 갈 수 있는 씬 생성 및 구현
    Title화면에서 버튼을 눌러 씬을 전환할수 있게 하였습니다 각 버튼에 Onclik()으로 작동되게해놓았음

--- Assets/Scripts/Manager/SceneChanger.cs
+++ Assets/Scripts/Manager/SceneChanger.cs
@@ -2,16 +2,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneChanger : Singleton<SceneChanger>
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
+    public void OnTitle() => SceneManager.LoadScene("TitleScene");
 
-    public void OnTitle() => SceneManager.LoadScene(0);
+    public void OnStartGame() => SceneManager.LoadScene("FirstCardChoiceScene");
 
-    public void OnStartGame() => SceneManager.LoadScene(1);
-
-    public void OnOption() => SceneManager.LoadScene(2);
-
+    public void OnOption() => SceneManager.LoadScene("OptionScene");
 }
